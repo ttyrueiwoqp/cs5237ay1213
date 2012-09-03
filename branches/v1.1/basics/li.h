7580d22803eb7d28548f7dd5b3e8cecdedfbdebe
lufangjiannus@gmail.com <lufangjiannus@gmail.com@379b93c5-e406-78fe-c0d7-a98afb2c5909>
2012-09-03 03:09:41 +0000

    Move undone codes to v1.1

---
+++ branches/v1.1/basics/li.h
@@ -0,0 +1,47 @@
+#ifndef LIH
+#define LIH
+
+#include <vector>
+using namespace std;
+
+class LongInt {
+
+	private:
+		vector<int> num; // int should be 4 digits
+		const static int DIVIDER = 10000; // for vector operation
+
+	protected:
+		void setZero_(); // set this number to be zero
+
+	public:
+		LongInt();
+		LongInt(LongInt&); // copy constructor
+		LongInt(int);      // take in an integer 
+		void dump();       // print out this number
+
+		LongInt& operator=(int); 
+		LongInt& operator=(LongInt&);  
+		LongInt operator+(LongInt&); 
+		LongInt operator+(int i) {return (*this) + LongInt(i);};
+		LongInt operator-(LongInt&);
+		LongInt operator-(int i) {return (*this) + LongInt(i);};
+		LongInt operator*(LongInt&);
+		LongInt operator*(int i) {return (*this) * LongInt(i);};
+		
+		bool operator>(LongInt&);
+		bool operator<(LongInt&);
+		bool operator==(LongInt&);
+
+		bool eqZero();  // return true if it is zero
+		int sign();     // return +1 if it's positive, -1 if it's negative, 0 if it's zero
+
+
+		double doubleValue(); // return a double approximation (you can assume that it will not be out of bound)
+
+	friend LongInt operator-(LongInt&); 
+};
+
+LongInt operator-(LongInt&); // negative sign
+
+
+#endif
