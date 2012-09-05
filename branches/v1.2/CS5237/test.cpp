9bf8741fa22097717801e5780b58df03b4f73504
lufangjiannus@gmail.com <lufangjiannus@gmail.com@379b93c5-e406-78fe-c0d7-a98afb2c5909>
2012-09-05 01:32:48 +0000

    Create v1.2 from v1.1 for bitwise optimization.

---
+++ branches/v1.2/CS5237/test.cpp
@@ -0,0 +1,208 @@
+/*
+
+	For this file, it should be sufficient to modify the function "processFile()" only.
+
+	*/
+
+
+#include <windows.h>
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <strstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+#include "li.h"
+#include "lmath.h"
+#include "pointSet.h"
+#include "stopWatch.h"
+
+static StopWatch globalSW;
+static string groupName = "(Your one-line group name here)";
+
+void processFile(char* ifilename, char* ofilename){
+
+	string line_noStr;
+
+	string line;   // each line of the file
+	string command;// the command of each line
+	string numberStr; // for single LongInt operation
+	string outputAns = "Answer of your computation"; // the answer you computed
+	LongInt output;
+
+	ifstream inputFile(ifilename,ios::in);
+	ofstream outputFile(ofilename,ios::out);
+
+	if(inputFile.fail()){
+		cerr << "Error: Cannot read input file \"" << ifilename << "\"";
+		exit(1);
+	}
+	/*
+	LongInt a(1);
+	LongInt b("1234567890");
+	LongInt c = a * b;
+	c.dump();
+	*/
+	while(inputFile.good()){
+
+		getline(inputFile,line);
+		if(line.empty()) {
+			command = "";
+			continue; 
+		}// in case the line has nothing in it
+
+		stringstream linestream(line);
+
+		linestream >> line_noStr;
+		linestream >> command;         // get the command
+
+		if(!command.compare("LX")){
+			linestream >> numberStr;
+
+			LongInt num(numberStr);
+			output = num;
+			outputAns = output.toString();
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("AD")){
+			linestream >> numberStr;
+
+			LongInt num(numberStr);
+			output = output + num;
+			outputAns = output.toString();
+			
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("SB")){
+			linestream >> numberStr;
+			
+			LongInt num(numberStr);
+			output = output - num;
+			outputAns = output.toString();
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("MU")){
+			linestream >> numberStr;
+			
+			LongInt num(numberStr);
+			output = output * num;
+			outputAns = output.toString();
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("NG")){
+			linestream >> numberStr;
+			
+			output = -output;
+			outputAns = output.toString();
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("SD")){
+			// you can assume that the three rows of matrix are in three seperate lines in the file
+			linestream >> numberStr;
+			getline(inputFile,line);		
+			getline(inputFile,line);		
+			getline(inputFile,line);
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("AP")){
+			linestream >> numberStr;
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("IT")){
+			linestream >> numberStr;
+
+			linestream >> numberStr;
+
+			linestream >> numberStr;
+
+			linestream >> numberStr;
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else if(!command.compare("IC")){
+			linestream >> numberStr;
+
+			linestream >> numberStr;
+
+			linestream >> numberStr;
+
+			linestream >> numberStr;
+
+			globalSW.pause();
+			outputFile << line_noStr  << " " << outputAns << endl;
+			globalSW.resume();
+		}
+		else{
+			cerr << "Exception: Wrong input command" << endl;
+		}
+	}
+	
+}
+
+void runningExperiment()
+{
+	globalSW.reset();
+	globalSW.resume();
+	processFile("input100.txt", "output.txt");
+	globalSW.pause();
+}
+
+int main()
+{
+    SYSTEMTIME st;
+	const int no_exp = 5;
+	ULONGLONG t[no_exp];
+	ULONGLONG total_t = 0;
+    
+    GetLocalTime(&st);
+	int start = (((st.wHour*60+st.wMinute)*60)+st.wSecond)*1000+st.wMilliseconds;
+
+	cerr << groupName <<endl;
+	cerr << "Start Time:  " << st.wHour << ":" << st.wMinute << ":" << st.wSecond + (st.wMilliseconds/1000.0) << endl;
+
+	for(int i=0;i<no_exp;i++)
+	{
+		runningExperiment();
+		t[i] = globalSW.ms();
+		total_t += t[i];
+	}
+
+    GetLocalTime(&st);
+	int end = (((st.wHour*60+st.wMinute)*60)+st.wSecond)*1000+st.wMilliseconds;
+	cerr << "Ending Time: " << st.wHour << ":" << st.wMinute << ":" << st.wSecond + (st.wMilliseconds/1000.0) << endl;
+	cerr << "Overall Length in millisecond(including file output): " << end-start << endl;
+	cerr << "Excluding file output time:"<< endl;
+		cerr << "         Total Stopwatch Length(ms): " << total_t << endl;
+
+	for(int i=0;i<no_exp;i++)
+		cerr << "         Stopwatch" << (i+1) << "Length(ms): " << t[i] << endl;
+	cerr << "         Average Stopwatch Length(ms): " << total_t/no_exp << endl;
+   
+	system("pause");
+}
