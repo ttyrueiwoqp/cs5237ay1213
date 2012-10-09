37caeee4016dba6563067c7afa6888678dc79bd8
alexander.faxa@gmail.com <alexander.faxa@gmail.com@379b93c5-e406-78fe-c0d7-a98afb2c5909>
2012-10-09 01:36:23 +0000

    Added files for phase 3.

---
+++ branches/v3.0/basics/li.cpp
@@ -0,0 +1,505 @@
+#include "li.h"
+#include <iostream>
+
+
+void LongInt::setZero_() {
+	num.clear();
+	num.push_back(0);
+	nSign = 0;
+}
+
+// add param, both positive
+void LongInt::add(const vector<int>& a, const vector<int>& b, vector<int>& result) {
+	int incr = 0;
+	int size = 0;
+	int p = (int) a.size();
+	int q = (int) b.size();
+	bool paramLonger = false;	
+	
+	// get the size of the shorter vector
+	if (p < q) {
+		size = p;
+		paramLonger = true;
+	} else {
+		size = q;
+	}
+	
+	// add the vectors from [0] to [size - 1]
+	for (int i = 0; i < size; i++) {
+		int sum = a[i] + b[i] + incr;
+		if (sum >= DIVIDER) {
+			incr = 1;
+			result.push_back(sum - DIVIDER);
+		} else {
+			incr = 0;
+			result.push_back(sum);
+		}
+	}
+
+	// add the rest by direct copying from the longer vector
+	if (!paramLonger) {
+		for (int i = size; i < p; i++) {
+			result.push_back(a[i]);
+		}
+	} else {
+		for (int i = size; i < q; i++) {
+			result.push_back(b[i]);
+		}
+	}
+	
+	// add the incr at [size]
+	if (incr == 1) {
+		if ((int) result.size() > size) {
+			result[size] += incr;
+		} else {
+			result.push_back(incr);
+		}
+	}
+}
+
+void LongInt::addAllVec(const vector<vector<int>>& a, vector<int>& result) {
+	
+	result.clear();
+	int totalSize = a.size();
+
+	// track the index of size
+	vector<int> tracker(totalSize);
+	for (int i = 0; i < totalSize; i++) {
+		tracker[i] = a[i].size() - 1;
+	}
+
+	int incr = 0;
+	int jEnd = (tracker[0] + ((totalSize + 2) >> 1) * NTOOM);
+	for (int j = 0; j <= jEnd; j++) {
+		int sum = incr % DIVIDER;
+		incr = incr / DIVIDER;
+
+		int iBegin = (j <= tracker[0]) ? 0 : (((j - tracker[0] - 1) / NTOOM) << 1) + 1;
+		int iEnd = ((j / NTOOM) << 1) + 1;
+		iEnd = (iEnd <= totalSize) ? iEnd : totalSize;
+		for (int i = iBegin; i < iEnd; i++) {
+
+			if (j <= tracker[i] && a[i][j] != 0) {
+				int temp = sum + a[i][j];
+				sum = temp % DIVIDER;
+				incr += temp / DIVIDER;
+			}
+		}
+		result.push_back(sum);
+	}
+	result.push_back(incr);
+}
+
+// subtract param, both positive, assume abs(a) > abs(b)
+void LongInt::subtract(const vector<int>& a, const vector<int>& b, vector<int>& result) {
+
+	int decr = 0;
+	int p = (int) a.size();
+	int q = (int) b.size();
+	bool paramLonger = false;
+	
+	// subtract the vectors from [0] to [q - 1]
+	for (int i = 0; i < q; i++) {
+		int sum = a[i] - decr - b[i];
+		if (sum < 0) {
+			decr = 1;
+			result.push_back(sum + DIVIDER);			
+		} else {
+			decr = 0;
+			result.push_back(sum);
+		}
+	}
+
+	// add the rest by direct copying from a
+	for (int i = q; i < p; i++) {
+		result.push_back(a[i]);
+	}
+	
+	// deduct the decr at [q]
+	// decr == 1 implies that result[q] is valid, since abs(a) > abs(b)
+	if (decr == 1) {
+		result[q] -= decr;
+	}
+
+	removeLeadingZeros(result);
+}
+
+/***********************************************************************
+	Using Toom-Cook multiplication
+	Only consider positive long int a, b
+	0 is handled by caller
+************************************************************************/
+void LongInt::multiply(const vector<int>& paramA, const vector<int>& paramB, vector<int>& result) {
+
+	vector<vector<int>> prod;
+	vector<int> a(paramA), b(paramB);
+
+	// add 0s to make the vector size of a, b multiples of NTOOM
+	while (a.size() % NTOOM != 0)
+		a.push_back(0);
+	while (b.size() % NTOOM != 0)
+		b.push_back(0);
+
+	int p = a.size();
+	int q = b.size();
+
+	for (int j = 0; j < q; j += NTOOM) {
+
+		vector<int> m(j), n(j + NTOOM);
+		
+		for (int i = 0; i < p; i += NTOOM) {
+			
+			if ((i & 1) == 0)
+				toom_3(a[i], a[i+1], a[i+2], b[j], b[j+1], b[j+2], m);
+			else
+				toom_3(a[i], a[i+1], a[i+2], b[j], b[j+1], b[j+2], n);
+		}
+		prod.push_back(m);
+		prod.push_back(n);
+	}
+
+	addAllVec(prod, result);
+	removeLeadingZeros(result);
+}
+
+void LongInt::toom_3(int a0, int a1, int a2, int b0, int b1, int b2, vector<int>& result) {
+
+	int p[5], q[5], r[5], s[5], t[5];
+
+	int pt = a0 + a2;
+	p[0] = a0;
+	p[1] = pt + a1;
+	p[2] = pt - a1; //p(-1)
+	p[3] = ((p[2] + a2) << 1) - a0; //p(-2)
+	p[4] = a2;
+
+	int qt = b0 + b2;
+	q[0] = b0;
+	q[1] = qt + b1;
+	q[2] = qt - b1; //q(-1)
+	q[3] = ((q[2] + b2) << 1) - b0; //q(-2)
+	q[4] = b2;
+
+	for (int i = 0; i < 5; i++)
+		r[i] = p[i] * q[i];
+
+	t[0] = r[0];
+	t[4] = r[4];
+	t[3] = (r[3] - r[1]) / 3;
+	t[1] = (r[1] - r[2]) >> 1;
+	t[2] = (r[2] - r[0]);
+	t[3] = ((t[2] - t[3]) >> 1) + (r[4] << 1);
+	t[2] = t[2] + t[1] - r[4];
+	t[1] = t[1] - t[3];
+		
+	for (int i = 0; i < 5; i++) {
+		s[i] = t[i] / DIVIDER;
+		t[i] = t[i] % DIVIDER;
+	}
+
+	result.push_back(t[0]);
+	int incr = 0;
+	for (int i = 0; i < 4; i++) {
+		int temp = s[i] + t[i + 1] + incr;
+		incr = temp / DIVIDER;
+		result.push_back(temp % DIVIDER);
+	}
+	result.push_back(s[4] + incr);
+
+}
+
+// check abs values, 1 if greater, 0 if equal, -1 if less
+int LongInt::absCompare(const vector<int>& a, const vector<int>& b) {
+	int p = (int) a.size();
+	int q = (int) b.size();
+
+	if (p > q) {
+		return 1;
+	} else if (p < q) {
+		return -1;
+	} else {
+		for (int i = p - 1; i >= 0; i--) {
+			if (a[i] > b[i]){
+				return 1;
+			} else if (a[i] < b[i]) {
+				return -1;
+			}
+		}
+		return 0; // equal
+	}
+}
+
+// remove the leading zeros
+void LongInt::removeLeadingZeros(vector<int>& param) {
+
+	int size = param.size();
+	for (int i = size - 1; i > 0; i--) {
+		if (param[i] == 0)
+			param.pop_back();
+		else
+			break;
+	}
+}
+
+LongInt::LongInt() {
+	num.clear();
+	nSign = 0;
+}
+
+LongInt::LongInt(const LongInt& param) {
+	num = param.num;
+	nSign = param.nSign;
+}
+
+/*****************************************************
+	int, e.g. 1234567890 is stored in num as follows:
+	num[0] = 7890;
+	num[1] = 3456;
+	num[2] = 12;
+	nSign = 1;
+*****************************************************/
+LongInt::LongInt(int i) {
+	num.clear();
+
+	if (i > 0) {
+		nSign = 1;
+	} else if (i < 0) {
+		nSign = -1;
+		i = 0 - i;
+	} else {
+		nSign = 0;
+	}
+
+	while (i >= DIVIDER) {
+		num.push_back(i % DIVIDER);
+		i /= DIVIDER;
+	}
+	num.push_back(i % DIVIDER);
+}
+
+LongInt::LongInt(string numStr) {
+	num.clear();
+	if (numStr.empty()) {
+		nSign = 0;
+	} else {
+		// delete minus sign
+		if (numStr[0] == '-') {
+			nSign = -1;
+			numStr.erase(0, 1);
+		} else {
+			nSign = 1;
+		}
+
+		// all chars except the first few
+		int size = (int) numStr.size();
+		for (int i = size - NDIGIT; i >= 0; i -= NDIGIT) {
+			string subStr = numStr.substr(i, NDIGIT);
+			num.push_back(atoi(subStr.c_str()));
+		}
+
+		// first few char
+		size = size % NDIGIT;
+		if (size != 0)
+			num.push_back(atoi(numStr.substr(0, size).c_str()));
+	}
+}
+
+void LongInt::dump() {
+	cout << toString() << endl;
+}
+
+LongInt& LongInt::operator=(int i) {
+	LongInt temp(i);
+	num = temp.num;
+	nSign = temp.nSign;
+	return *this;
+}
+
+LongInt& LongInt::operator=(const LongInt& param) {
+	num = param.num;
+	nSign = param.nSign;
+	return *this;
+}
+
+LongInt LongInt::operator+(const LongInt& param) {
+	LongInt result;
+
+	if (eqZero()) {
+		result = LongInt(param);
+	} else if (param.eqZero()) {
+		result = *this;
+	} else if (sign() == param.sign()) {
+		add(num, param.num, result.num);
+		result.nSign = sign();
+	} else {
+		// different sign, subtract
+		int temp = absCompare(num, param.num);
+		if (temp == 0) {
+			result.setZero_();
+		} else if (temp > 0) {
+			subtract(num, param.num, result.num);
+			result.nSign = sign();
+		} else {
+			subtract(param.num, num, result.num);
+			result.nSign = 0 - sign();
+		}
+	}
+
+	return result;			
+}
+
+LongInt LongInt::operator-(const LongInt& param) {
+	LongInt result;
+
+	if (eqZero()) {
+		result.num = param.num;
+		result.nSign = 0 - param.sign();
+	} else if (param.eqZero()) {
+		result = *this;
+	} else if (sign() == param.sign()) {
+		// same sign, subtract
+		int temp = absCompare(num, param.num);
+		if (temp == 0) {
+			result.setZero_();
+		} else if (temp > 0) {
+			subtract(num, param.num, result.num);
+			result.nSign = sign();
+		} else {
+			subtract(param.num, num, result.num);
+			result.nSign = 0 - sign();
+		}
+
+	} else {
+		add(num, param.num, result.num);
+		result.nSign = sign();
+	}
+
+	return result;
+}
+
+LongInt LongInt::operator*(const LongInt& param) {
+	LongInt result;
+
+	if (eqZero() || param.eqZero())	{
+		result.setZero_();
+		return result;
+	}
+	
+	multiply(num, param.num, result.num);
+	result.nSign = sign() * param.sign();
+
+	return result;
+}
+
+bool LongInt::operator>(const LongInt& param) {
+	if (sign() > param.sign())
+		return true;
+	else if (sign() < param.sign())
+		return false;
+	else if (sign() == 0)
+		return false;
+
+	int a = (int) num.size();
+	int b = (int) param.num.size();
+	int result = absCompare(num, param.num);
+
+	return (sign() == result) ? true : false;
+}
+
+bool LongInt::operator<(const LongInt& param) {
+	if (sign() < param.sign())
+		return true;
+	else if (sign() > param.sign())
+		return false;
+	else if (sign() == 0)
+		return false;
+
+	int a = (int) num.size();
+	int b = (int) param.num.size();
+	int result = absCompare(num, param.num);
+
+	return (sign() == 0 - result) ? true : false;
+}
+
+bool LongInt::operator==(const LongInt& param) {
+	if (sign() != param.sign())
+		return false;
+
+	int a = (int) num.size();
+	int b = (int) param.num.size();
+
+	if (a != b) {
+		return false;
+	} else {
+		for (int i = a - 1; i >= 0; i--) {
+			if (num[i] != param.num[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+bool LongInt::eqZero() const {
+	if (sign() == 0)
+		return true;
+
+	int size = (int) num.size();
+	for (int i = size - 1; i >= 0; i--) {
+		if (num[i] != 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int LongInt::sign() const {
+	return nSign;
+}
+
+double LongInt::doubleValue() {
+	double result = 0.0;
+	int size = (int) num.size();
+	int incr = 1;
+	for (int i = 0; i < size; i++) {
+		result += num[i] * incr;
+		incr *= DIVIDER;
+	}
+	if( (nSign==-1) && result > 0.0) 
+		result = result * -1.0;
+	return result;
+}
+
+string LongInt::toString() {
+	string zero = "0";
+	if (sign() == 0) {
+		return zero;
+	}
+
+	string result;
+	char buffer[100];
+	int size = (int) num.size();
+	for (int i = 0; i < size - 1; i++) {
+		string s = _itoa(num[i], buffer, 10);
+		while (s.size() < NDIGIT)
+			s = zero + s;
+		result = s + result;
+	}
+
+	// first few chars
+	string first = _itoa(num[size - 1], buffer, 10);
+	result = first + result;
+	
+	// minus sign
+	if (sign() == -1) {
+		string minus = "-";
+		result = minus + result;
+	}
+	return result;
+}
+
+LongInt operator-(const LongInt& param) {
+	LongInt result(param);
+	result.nSign = 0 - param.sign();
+	return result;
+}
---
+++ branches/v3.0/main.cpp
@@ -0,0 +1,407 @@
+
+#include "basicsP2\pointSetArray.h"
+#include "basicsP2\Trist.h"
+
+#include "math.h"
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <cstdio>
+#include <fstream>
+#include <strstream>
+#include <string>
+#include <sstream>
+
+#include "GL\glut.h"
+using namespace std;
+
+
+#define T2C				1		//Tranform to center
+double WinWidth = 1000;
+double WinHeight = 700;
+double HalfWinWidth	 = (WinWidth/2);
+double HalfWinHeight = (WinHeight/2);
+
+float scaleVal = 1.0;
+PointSetArray points; // The points added by the user.
+Trist triangles; // The current triangles
+
+// These three functions are for those who are not familiar with OpenGL, you can change these or even completely ignore them
+
+void drawAPoint(double x,double y)
+{
+	glPointSize(5);
+	glBegin(GL_POINTS);
+	glColor3f(0,0,0);
+#if T2C
+	x *= scaleVal;
+	y *= scaleVal;	
+	x += HalfWinWidth;
+	y += HalfWinHeight;
+#endif
+	glVertex2d(x,y);
+	glEnd();
+	glPointSize(1);
+}
+
+void drawALine(double x1,double y1, double x2, double y2)
+{
+	glPointSize(1);
+	glBegin(GL_LINE_LOOP);
+	glColor3f(0,0,1);
+#if T2C
+	x1 *= scaleVal;
+	y1 *= scaleVal;
+	x2 *= scaleVal;
+	y2 *= scaleVal;
+
+	x1 += HalfWinWidth;
+	y1 += HalfWinHeight;
+	x2 += HalfWinWidth;
+	y2 += HalfWinHeight;
+#endif
+	glVertex2d(x1,y1);
+	glVertex2d(x2,y2);
+	glEnd();
+	glPointSize(1);
+}
+
+void drawATriangle(double x1,double y1, double x2, double y2, double x3, double y3)
+{
+	glBegin(GL_POLYGON);
+
+	//to set randon color for all triangle
+	double col1, col2, col3;
+	col1 = (double)rand() /RAND_MAX;
+	col2 = (double)rand() /RAND_MAX; 
+	col3 = (double)rand() /RAND_MAX; 
+	glColor3f( col1, col2, col3);
+
+#if T2C
+	x1 *= scaleVal;
+	y1 *= scaleVal;
+	x2 *= scaleVal;
+	y2 *= scaleVal;
+	x3 *= scaleVal;
+	y3 *= scaleVal;
+
+	x1 += HalfWinWidth;
+	y1 += HalfWinHeight;
+	x2 += HalfWinWidth;
+	y2 += HalfWinHeight;
+	x3 += HalfWinWidth;
+	y3 += HalfWinHeight;
+#endif
+	glVertex2d(x1, y1);
+	glVertex2d(x2, y2);
+	glVertex2d(x3, y3);
+	glEnd();
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+
+	//print all points
+	points.print();
+
+	// Draw all points
+	for (int i = 1; i <= points.noPt(); i++)
+	{
+		LongInt x, y;
+		int res = points.getPoint(i, x, y);
+		if (res != 1)
+			cout << "Error, wrong point index" << endl;
+		else
+			drawAPoint(x.doubleValue(), y.doubleValue());
+	}
+
+	// Draw all triangles
+	for ( int i = 0; i < triangles.noTri(); i++)
+	{
+		int v1, v2, v3;
+
+		// How can I get a proper OpTri from here? i<< 3 looks wrong.
+		if( triangles.getVertexIdx(i << 3, v1, v2, v3) )
+		{
+			LongInt x1, y1, x2, y2, x3, y3;
+			int res1 = points.getPoint(v1, x1, y1);
+			int res2 = points.getPoint(v2, x2, y2);
+			int res3 = points.getPoint(v3, x3, y3);
+
+			if (res1 != 1 || res2 != 1 || res3 != 1)
+				cout << "Error, wrong triangle point index" << endl;
+			else
+			{
+				drawATriangle(x1.doubleValue(), y1.doubleValue(),
+					x2.doubleValue(), y2.doubleValue(),
+					x3.doubleValue(), y3.doubleValue());
+
+				// Draw triangle outline
+				drawALine(x1.doubleValue(), y1.doubleValue(), x2.doubleValue(), y2.doubleValue());
+				drawALine(x2.doubleValue(), y2.doubleValue(), x3.doubleValue(), y3.doubleValue());
+				drawALine(x3.doubleValue(), y3.doubleValue(), x1.doubleValue(), y1.doubleValue());
+			}
+		}
+	}
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void processIP(LongInt x, LongInt y)
+{
+	bool bInTri = false;
+	int ptIdx = points.checkPointExist(x, y);
+	if( ptIdx < 1 )
+	{
+		int PtSz = points.addPoint(x, y); // add the point to the list of points
+		// Iterator thru' all triangles to check for intri
+		for ( int i = 0; i < triangles.noTri(); i++)
+		{
+			int v1, v2, v3, v4;
+			v4 = PtSz;
+			if( triangles.getVertexIdx(i << 3, v1, v2, v3) )
+			{
+				//Click inside one of the triangles
+				if( PointSet::inTri(v1, v2, v3, v4) == 1 )
+				{
+					bInTri = true;
+				//	points.print();
+					triangles.makeTri3(i, v4);
+					break;
+				}
+			}
+		}
+		//Click outside all the triangles, so remove the last point
+		if( bInTri == false )
+		{
+			points.eraseLastPoint();
+		}
+	}
+	else
+	{
+		//remove the points and the triangles that connected
+		triangles.delTriPt(ptIdx);
+	}
+}
+
+void reshape (int w, int h)
+{
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+	WinWidth = w;
+	WinHeight = h;
+	HalfWinWidth = (WinWidth/2);
+	HalfWinHeight = (WinHeight/2);
+
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,w,h,0);  
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void init(void)
+{
+	glClearColor (1.0,1.0,1.0, 1.0);
+}
+
+void readFile(){
+
+	string line_noStr;
+
+	string line;   // each line of the file
+	string command;// the command of each line
+	string numberStr; // for single LongInt operation
+	string outputAns = "Answer of your computation"; // the answer you computed
+
+	ifstream inputFile("input.txt",ios::in);
+
+	LongInt li;
+
+	if(inputFile.fail()){
+		cerr << "Error: Cannot read input file \"" << "input.txt" << "\"";
+		return;
+	}
+
+	//Clear data trist records
+	triangles.clear();
+	points.eraseAllPoints();
+
+	while(inputFile.good()){
+		getline(inputFile,line);
+		if(line.empty()) {
+			command = "";
+			continue; 
+		}// in case the line has nothing in it
+
+		stringstream linestream(line);
+
+		linestream >> line_noStr;
+		linestream >> command;         // get the command
+
+		if(!command.compare("AP")){
+			linestream >> numberStr;
+			LongInt x, y;
+			x = LongInt(numberStr);
+			linestream >> numberStr;
+			y = LongInt(numberStr);
+
+			li = points.addPoint(x, y);
+		}
+		else if(!command.compare("OT")){
+			int v1, v2, v3;
+			linestream >> numberStr;
+			v1 = atoi(numberStr.c_str());
+			linestream >> numberStr;	
+			v2 = atoi(numberStr.c_str());
+			linestream >> numberStr;
+			v3 = atoi(numberStr.c_str());
+
+			triangles.makeTri(v1, v2, v3, true);
+		}
+		else if(!command.compare("IP")){
+			linestream >> numberStr;
+			LongInt x, y;
+			x = LongInt(numberStr);
+			linestream >> numberStr;
+			y = LongInt(numberStr);
+
+			processIP(x, y);
+		}
+		else if(!command.compare("DY")){ 
+			linestream >> numberStr;
+		}
+		else{
+			cerr << "Exception: Wrong input command" << endl;
+		}
+	}
+
+}
+
+void writeFile()
+{
+	ofstream outputFile("savefile.txt",ios::out);
+	if(outputFile.fail()){
+		cerr << "Error: Cannot create output file \"" << "output.txt" << "\"";
+		return;
+	}
+
+	int lineNbr = 0;
+
+	// Add all points to output file
+	for (int i = 1; i <= points.noPt(); i++)
+	{
+		string prefix = (lineNbr < 10) ? "000" :
+			(lineNbr < 100) ? "00" :
+			(lineNbr < 1000) ? "0" : "";
+
+		LongInt x, y;
+		int res = points.getPoint(i, x, y);
+		if (res != 1)
+			cout << "Error, wrong point index" << endl;
+		else
+		{
+			outputFile << prefix << lineNbr++ << ": AP " << x.toString() << " " << y.toString() << endl;
+		}
+	}
+
+	// Add all triangles to output file
+	for (int i = 0; i < triangles.noTri(); i++)
+	{
+		string prefix = (lineNbr < 10) ? "000" :
+			(lineNbr < 100) ? "00" :
+			(lineNbr < 1000) ? "0" : "";
+
+		int v1, v2, v3;	
+		if( triangles.getVertexIdx(i << 3, v1, v2, v3) )
+		{
+			outputFile << prefix << lineNbr++ << ": OT " << v1 << " " << v2 << " " << v3 << endl;
+		}
+	}
+}
+
+void keyboard (unsigned char key, int x, int y)
+{
+	switch (key) {
+		case 'r':
+		case 'R':
+			readFile();
+			break;
+
+		case 'w':
+		case 'W':
+			writeFile();
+			break;
+
+		case 'Q':
+		case 'q':
+			exit(0);
+			break;
+
+		case '+':
+			scaleVal += 0.1;
+			break;
+
+		case '-':
+			if( scaleVal > 0.1 ) 
+				scaleVal -= 0.1;
+			break;
+
+		case '.':
+			scaleVal = 1.0;
+			break;
+		default:
+			break;
+	}
+
+	glutPostRedisplay();
+}
+
+
+void mouse(int button, int state, int x, int y)
+{
+	/*button: GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, or GLUT_RIGHT_BUTTON */
+	/*state: GLUT_UP or GLUT_DOWN */
+	enum
+	{
+		MOUSE_LEFT_BUTTON = 0,
+		MOUSE_MIDDLE_BUTTON = 1,
+		MOUSE_RIGHT_BUTTON = 2,
+		MOUSE_SCROLL_UP = 3,
+		MOUSE_SCROLL_DOWN = 4
+	};
+	if((button == MOUSE_RIGHT_BUTTON)&&(state == GLUT_UP))
+	{
+#if T2C
+		x -= HalfWinWidth;
+		y -= HalfWinHeight;
+		x /= scaleVal;
+		y /= scaleVal;
+#endif
+		processIP(LongInt(x), LongInt(y));
+		glutPostRedisplay();
+	}
+}
+
+int main(int argc, char **argv)
+{
+	cout<<"CS5237 Phase II"<< endl<< endl;
+	cout << "Right mouse click: OT operation"<<endl;
+	cout << "Q: Quit" <<endl;
+	cout << "R: Read in control points from \"input.txt\"" <<endl;
+	cout << "W: Write control points to \"savefile.txt\"" <<endl;
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize (WinWidth, WinHeight);
+	glutInitWindowPosition (50, 50);
+	glutCreateWindow ("CS5237 Phase II");
+	init ();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+
+	return 0;
+}
