3597a1f7c7b91557da885697b8fd5bd3ce81fa8b
lufangjiannus@gmail.com <lufangjiannus@gmail.com@379b93c5-e406-78fe-c0d7-a98afb2c5909>
2012-11-09 13:15:22 +0000

    Branch out rev 64 to v4.1 since I am not able to compile PK's code.

---
+++ branches/v4.1/basicsP2/pointSetArray.h
@@ -0,0 +1,20 @@
+#ifndef POINTSETARRAYH
+#define POINTSETARRAYH
+
+#include "..\basics\pointSet.h"
+
+using namespace std;
+
+class PointSetArray : public PointSet {
+
+public:
+
+	int getPoint (int pIndex, LongInt& x1,LongInt& y1); // put the x,y values into x1,y1, and return 1 if the point pIndex exists
+	int noPt();                                         // return the number of points
+	void eraseAllPoints();                              // erase all points in this array
+	void print();										// for debugging only
+	void eraseLastPoint();								// remove the last point when it fail in tri test
+	int checkPointExist(LongInt& x1,LongInt& y1);		// check if the point exist return point idx otherwise -1 
+};
+
+#endif
